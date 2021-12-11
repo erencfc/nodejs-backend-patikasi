2f704f168f98e55d512548812a40cfa2e68c9cb4
Eren Çifci <eren.cifci04@gmail.com>
2021-12-11 19:04:25 +0300

    MongoDB ile post paylaşma ve anasayfada görüntüleme eklendi.

--- Node.js/Projeler-ve-Odevler/Proje1-Clean-Blog/app.js
+++ Node.js/Projeler-ve-Odevler/Proje1-Clean-Blog/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const app = express();
+const Post = require("./models/Post");
+
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
 
 app.set("view engine", "ejs");
 
+// MIDDLEWARES
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/", async (req, res) => {
+    const posts = await Post.find({});
+    res.render("index", {
+        posts,
+    });
 });
 
 app.get("/about", (req, res) => {
@@ -18,12 +28,17 @@ app.get("/post", (req, res) => {
     res.render("post");
 });
 
-app.get("/add_post", (req, res) => {
-    res.render("add_post");
+app.get("/add", (req, res) => {
+    res.render("add");
 });
 
 app.get("*", (req, res) => {
     res.writeHead(404, "text/html").end("<h1>404 NOT FOUND</h1>");
+});
+
+app.post("/posts", async (req, res) => {
+    await Post.create(req.body);
+    res.redirect("/");
 });
 
 const PORT = 80;
