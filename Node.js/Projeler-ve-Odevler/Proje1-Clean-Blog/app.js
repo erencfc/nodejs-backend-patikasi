9ec6e2a69ca77d108d31625c8346f447b35314f2
Eren Çifci <eren.cifci04@gmail.com>
2021-12-12 00:49:56 +0300

    Post editleme oluşturuldu ve post sayfasına butonlar eklendi.

--- Node.js/Projeler-ve-Odevler/Proje1-Clean-Blog/app.js
+++ Node.js/Projeler-ve-Odevler/Proje1-Clean-Blog/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const ejs = require("ejs");
 const app = express();
 const Post = require("./models/Post");
@@ -12,9 +13,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.get("/", async (req, res) => {
-    const posts = await Post.find({});
+    const posts = await Post.find({}).sort("-dateCreated");
     res.render("index", {
         posts,
     });
@@ -25,6 +27,11 @@ app.get("/posts/:id", async (req, res) => {
     res.render("post", {
         post,
     });
+});
+
+app.get("/posts/edit/:id", async (req, res) => {
+    const post = await Post.findOne({ _id: req.params.id });
+    res.render("edit", { post });
 });
 
 app.get("/about", (req, res) => {
@@ -46,6 +53,14 @@ app.get("*", (req, res) => {
 app.post("/posts", async (req, res) => {
     await Post.create(req.body);
     res.redirect("/");
+});
+
+app.put("/posts/:id", async (req, res) => {
+    const post = await Post.findOne({ _id: req.params.id });
+    post.title = req.body.title;
+    post.detail = req.body.detail;
+    post.save();
+    res.redirect(`/posts/${post._id}`);
 });
 
 const PORT = 80;
