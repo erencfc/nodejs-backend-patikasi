cf00ddf46c37804342bf090b5a6da4e09a19dd99
Eren Çifci <eren.cifci04@gmail.com>
2021-12-12 20:10:34 +0300

    Delete temp

--- Node.js/Projeler-ve-Odevler/Proje2-SmartEduStarter/app.js
+++ Node.js/Projeler-ve-Odevler/Proje2-SmartEduStarter/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+// MIDDLEWARES
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.status("200").send("INDEX SAYFASI");
 });
