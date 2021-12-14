d698188e89476a344ce039aa738f7892d70f9ec9
Eren Çifci <eren.cifci04@gmail.com>
2021-12-15 00:05:24 +0300

    Kurs alma ve bırakma eklendi, dashboard düzenlendi.

--- Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/controllers/authController.js
+++ Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/controllers/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const Category = require("../models/Category");
+const Course = require("../models/Course");
 
 exports.createUser = async (req, res) => {
     try {
@@ -20,11 +21,9 @@ exports.loginUser = (req, res) => {
         User.findOne({ email: email }, (err, user) => {
             if (user) {
                 bcrypt.compare(password, user.password, (err, same) => {
-                    if (same) {
-                        // USER SESSION
-                        req.session.userID = user._id;
-                        res.status(200).redirect("/users/dashboard");
-                    }
+                    // USER SESSION
+                    req.session.userID = user._id;
+                    res.status(200).redirect("/users/dashboard");
                 });
             }
             if (err) console.log(err);
@@ -41,11 +40,15 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-    const user = await User.findOne({ _id: req.session.userID });
+    const user = await User.findOne({ _id: req.session.userID }).populate(
+        "courses"
+    );
     const categories = await Category.find();
+    const courses = await Course.find({ user: req.session.userID });
     res.status(200).render("dashboard", {
         page_name: "dashboard",
         user: user,
         categories: categories,
+        courses: courses,
     });
 };
