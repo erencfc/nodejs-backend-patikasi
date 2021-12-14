b2bb0137f6f86870c438c623988e820adef1b9ed
Eren Çifci <eren.cifci04@gmail.com>
2021-12-14 19:02:18 +0300

    Öğretmenler için dashboardda kurs oluştur eklendi.

--- Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/routes/courseRoute.js
+++ Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/routes/courseRoute.js
@@ -4,10 +4,11 @@ const {
     getAllCourses,
     getCourse,
 } = require("../controllers/courseController");
+const roleMiddleware = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(createCourse);
+router.route("/").post(roleMiddleware(["teacher", "admin"]), createCourse);
 router.route("/").get(getAllCourses);
 router.route("/:slug").get(getCourse);
 
