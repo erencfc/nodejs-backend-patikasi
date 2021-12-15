45203b11692b72894c9b06023466834b2d2c15ce
Eren Çifci <eren.cifci04@gmail.com>
2021-12-15 18:58:25 +0300

    Search eklendi + courses sayfasında teacher name düzenlendi.

--- Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/controllers/courseController.js
+++ Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/controllers/courseController.js
@@ -22,6 +22,8 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
     try {
         const categorySlug = req.query.categories;
+        const query = req.query.search;
+
         const category = await Category.findOne({ slug: categorySlug });
 
         let filter = {};
@@ -30,7 +32,23 @@ exports.getAllCourses = async (req, res) => {
             filter = { category: category._id };
         }
 
-        const courses = await Course.find(filter).sort("-createdAt");
+        if (query) {
+            filter = { name: query };
+        }
+
+        if (!query && !categorySlug) {
+            filter.name = "";
+            filter.category = null;
+        }
+
+        const courses = await Course.find({
+            $or: [
+                { name: { $regex: ".*" + filter.name + ".*", $options: "i" } },
+                { category: filter.category },
+            ],
+        })
+            .sort("-createdAt")
+            .populate("user");
         const categories = await Category.find();
         res.status(200).render("courses", {
             courses: courses,
@@ -51,11 +69,13 @@ exports.getCourse = async (req, res) => {
         const course = await Course.findOne({ slug: req.params.slug }).populate(
             "user"
         );
+        const categories = await Category.find();
 
         res.status(200).render("course", {
-            course,
+            course: course,
             page_name: "courses",
             user: user,
+            categories: categories,
         });
     } catch (error) {
         res.status(400).json({
