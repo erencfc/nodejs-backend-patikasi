014ebf46ff4fc87dcf99670a9d1e5c28d76cbe7b
Eren Çifci <eren.cifci04@gmail.com>
2021-12-16 21:38:13 +0300

    Flash errorlar express-validator ile güncellendi.

--- Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/controllers/authController.js
+++ Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/controllers/authController.js
@@ -1,65 +1,59 @@
 const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+
 const User = require("../models/User");
 const Category = require("../models/Category");
 const Course = require("../models/Course");
 
 exports.createUser = async (req, res) => {
-    if (!req.body.name || !req.body.email || !req.body.password) {
-        req.flash("error", "You must fill in the blanks!");
-        res.status(400).redirect("/register");
-    } else {
-        try {
-            await User.create(req.body);
-            req.flash("success", "Registered successfully.");
-            res.status(201).redirect("/login");
-        } catch (error) {
-            if (error.code == 11000) {
-                req.flash("error", "User already exists!");
-                res.status(400).redirect("/register");
-            } else {
-                req.flash(
-                    "error",
-                    "Something happened! We couldn't create your account."
-                );
-                res.status(400).redirect("/register");
-            }
+    try {
+        await User.create(req.body);
+        req.flash("success", "Registered successfully.");
+        res.status(201).redirect("/login");
+    } catch (error) {
+        const errors = validationResult(req);
+
+        for (let i = 0; i < errors.array().length; i++) {
+            req.flash("error", errors.array()[i].msg);
         }
+        res.status(400).redirect("/register");
     }
 };
 
 exports.loginUser = (req, res) => {
     const { email, password } = req.body;
-    if (!email.trim() || !password.trim()) {
-        req.flash("error", "You must fill in the blanks!");
-        res.status(400).redirect("/login");
-    } else {
-        try {
-            User.findOne({ email: email }, (err, user) => {
-                if (user) {
-                    bcrypt.compare(password, user.password, (err, same) => {
-                        if (same) {
-                            req.session.userID = user._id;
-                            res.status(200).redirect("/users/dashboard");
-                        } else {
-                            req.flash(
-                                "error",
-                                "Password is incorrect! Please try again."
-                            );
-                            res.status(400).redirect("/login");
-                        }
-                    });
-                } else {
-                    req.flash(
-                        "error",
-                        'User not found! Click<a href="/register"> here </a>to create an account.'
-                    );
-                    res.status(400).redirect("/login");
-                }
-                if (err) console.log(err);
-            });
-        } catch (err) {
-            console.log(err);
+
+    if (!email || !password) {
+        const errors = validationResult(req);
+        for (let i = 0; i < errors.array().length; i++) {
+            req.flash("error", errors.array()[i].msg);
         }
+        return res.status(400).redirect("/login");
+    }
+
+    try {
+        User.findOne({ email: email }, (err, user) => {
+            if (user) {
+                bcrypt.compare(password, user.password, (err, same) => {
+                    if (same) {
+                        req.session.userID = user._id;
+                        res.status(200).redirect("/users/dashboard");
+                    } else {
+                        req.flash("error", "Password is incorrect!");
+                        res.status(400).redirect("/login");
+                    }
+                });
+            } else {
+                req.flash(
+                    "error",
+                    'User not found! Click<a href="/register"> here </a>to create an account.'
+                );
+                res.status(400).redirect("/login");
+            }
+            if (err) console.log(err);
+        });
+    } catch (err) {
+        console.log(err);
     }
 };
 
--- Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/routes/userRoute.js
+++ Node.js/Projeler-ve-Odevler/Proje2-SmartEdu/routes/userRoute.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { body } = require("express-validator");
+
 const {
     createUser,
     loginUser,
@@ -6,11 +8,42 @@ const {
     getDashboardPage,
 } = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const User = require("../models/User");
 
 const router = express.Router();
 
-router.route("/signup").post(createUser);
-router.route("/login").post(loginUser);
+router.route("/signup").post(
+    [
+        body("name").not().isEmpty().withMessage("Please enter your name."),
+        body("email")
+            .isEmail()
+            .withMessage("Please enter a valid email.")
+            .custom((userEmail) => {
+                return User.findOne({ email: userEmail }).then((user) => {
+                    if (user) {
+                        return Promise.reject("The user already exists!");
+                    }
+                });
+            }),
+        body("password")
+            .not()
+            .isEmpty()
+            .withMessage("Please enter your password."),
+    ],
+    createUser
+);
+router
+    .route("/login")
+    .post(
+        [
+            body("email").isEmail().withMessage("Please enter your email."),
+            body("password")
+                .not()
+                .isEmpty()
+                .withMessage("Please enter your password."),
+        ],
+        loginUser
+    );
 router.route("/logout").get(logoutUser);
 router.route("/dashboard").get(authMiddleware, getDashboardPage);
 
