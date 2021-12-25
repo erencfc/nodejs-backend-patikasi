120fd37a6d0f31c26d0d28edc8b2f0300db45a76
Eren Çifci <eren.cifci04@gmail.com>
2021-12-25 23:16:45 +0300

    Apollo client kuruldu

---
+++ GraphQL/client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <div className="App">Selam</div>;
+}
+
+export default App;
---
+++ GraphQL/client/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { ApolloProvider } from "@apollo/client";
+
+import client from "./apollo";
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById("root")
+);
