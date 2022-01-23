26180a7283e2d410bc985a4689aebe7bd5b71b38
Eren Çifci <eren.cifci04@gmail.com>
2022-01-23 15:01:44 +0300

    add voting app project

---
+++ GraphQL/Voting-App-React/client/src/App.js
@@ -0,0 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+import Questions from "./pages/Questions";
+import New from "./pages/New";
+import Detail from "./pages/Detail";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Questions</Link>
+        <Link to="/new">New Question</Link>
+      </nav>
+      <hr />
+
+      <Routes>
+        <Route path="/" element={<Questions />} />
+        <Route path="new" element={<New />} />
+        <Route path="q/:id" element={<Detail />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ GraphQL/Voting-App-React/client/src/pages/Questions/index.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+import { useSubscription } from "@apollo/client";
+import { QUESTIONS_SUBSCRIPTION } from "./queries";
+
+import Loading from "../../components/Loading";
+
+function Questions() {
+  const { loading, data } = useSubscription(QUESTIONS_SUBSCRIPTION);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      {data.questions.map((question) => (
+        <div key={question.id}>
+          <Link to={`/q/${question.id}`}>{question.title}</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Questions;
